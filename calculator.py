4f4c08e0ae9619a59d5bbbb92d8e8d81d724be7e
nandiniiwork <nandiniiguptawork@gmail.com>
2023-07-03 23:33:21 +0530

    Taking inputs from the end user

---
+++ calculator.py
@@ -0,0 +1,5 @@
+print("Please select operation")
+print("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Exit")
+o = int(input("Select operations from 1, 2, 3, 4, 5 : "))
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
