c0d6c22332135b57cfe02d6cc7d5b90779c9368d
nandiniiwork <nandiniiguptawork@gmail.com>
2023-07-03 23:59:44 +0530

    putting entire code in while loop and adding break statement to exit the loop

--- calculator.py
+++ calculator.py
@@ -1,20 +1,23 @@
-print("Please select operation")
-print("1. Addition\n2. Substraction\n3. Multiplication\n4. Division\n5. Exit")
-o = int(input("Select operations from 1, 2, 3, 4, 5 : "))
-a = int(input("Enter first number : "))
-b = int(input("Enter second number : "))
+while True:
+    print("Please select operation")
+    print("1. Addition\n2. Substraction\n3. Multiplication\n4. Division\n5. Exit")
+    o = int(input("Select operations from 1, 2, 3, 4, 5 : "))
+    if o == 5:
+        break
+    a = int(input("Enter first number : "))
+    b = int(input("Enter second number : "))
 
-if o == 1:
-    print("Addition of", a, "and", b, "is", a+b)
-elif o == 2:
-    print("Substraction of", a, "and", b, "is", a-b)
-elif o == 3:
-    print("Multiplication of", a, "and", b, "is", a*b)
-elif o == 4:
+    if o == 1:
+        print("Addition of", a, "and", b, "is", a+b)
+    elif o == 2:
+        print("Substraction of", a, "and", b, "is", a-b)
+    elif o == 3:
+        print("Multiplication of", a, "and", b, "is", a*b)
+    elif o == 4:
         if b == 0:
             print("Division by zero is undefined")
         else:
             print("Division of", a, "and", b, "is", a/b)    
-else:
-    print("Invalid input")
+    else:
+        print("Invalid input")
               
